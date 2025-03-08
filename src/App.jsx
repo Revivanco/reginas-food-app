b7449e4b0f27ba337ffa26983817e1e2ba7cd868
Regina Vivanco Lemus <revilem1331@gmail.com>
2025-03-07 20:57:06 -0600

    Container and footer components added

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import { Box, Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <Box sx={{ border: "1px solid pink", width: 1850, height: 900 }}>
+    <Box sx={{ border: "1px solid pink", width: "100%", height: "100vh" }}>
 
-      {/* Contenedor superior a lo largo de la página */}
+      {/* primer contenedor */}
       <Container
         maxWidth={false}
         sx={{
@@ -34,12 +34,50 @@ function App() {
         </Typography>
       </Container>
 
-      <p>Mi contenedor</p>
-
+      {/* segundo contenedor */}
+      <Container
+        sx={{
+          border: "3px solid pink",  
+          width: "40%",              
+          position: "absolute",      
+          top: "15%",                
+          left: 0,                   
+          textAlign: "center",      
+          borderRadius: "8px",      
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",  
+        }}
+      >
+        <Typography
+          variant="body1"
+          sx={{
+            fontStyle: "italic",
+            fontSize: "18px",
+            color: "#333",
+            lineHeight: 1.6
+          }}
+        >
+          Siempre he creído que la comida es una de las mejores formas de disfrutar la vida. No soy chef, pero me apasiona descubrir lugares y platos deliciosos para compartir con los demás. Mi objetivo es recomendar experiencias gastronómicas que hagan que cada comida sea memorable. A través de mis recomendaciones, quiero que todos puedan disfrutar de sabores únicos y probar algo nuevo. Porque, aunque no cocine, mi amor por la buena comida y el deseo de compartirlo con ustedes es lo que me motiva a seguir buscando los mejores sabores para recomendar.
+        </Typography>
+      </Container>
+      {/* Footer */}
+      <Box
+        sx={{
+          backgroundColor: "#880E4F",
+          color: "#fff",
+          textAlign: "center",
+          padding: 2,
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          width: "100%",
+        }}
+      >
+        <Typography variant="body2" sx={{ fontStyle: "italic", fontSize: "14px" }}>
+          © 2025 Regina's Food. Todos los derechos reservados.
+        </Typography>
+      </Box>
     </Box>
   );
 }
 
 export default App;
-
-//box es el componente padre
